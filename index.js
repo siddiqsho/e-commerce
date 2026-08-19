78d1bf4233d179c87c6b178c4f66faebca63e41b
s1ddiq777 <sidprosto77@gmail.com>
2026-08-19 21:09:03 +0500

    Added buttons to cards, implemented the error handler, and created the header.

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 let parentCarts=document.querySelector('.parent-cards')
 
 fetch('https://fakestoreapi.com/products')
-.then(res=>res.json())
-.then(json=>{
-    json.forEach(product=> {
+.then(res=>{
+  if(!res.ok){
+    throw new Error('Error')
+  }
+  return res.json()
+})
+.then(products=>{
+    products.forEach(product=> {
       let card =document.createElement('div')
-      let forCard=document.createElement('div')
+      let cardInfo=document.createElement('div')
+      let cardButtons=document.createElement('div')
+
       card.classList.add('card')
-      forCard.classList.add('for-card')
+      cardInfo.classList.add('card-info')
+      cardButtons.classList.add('card-buttons')
+
 
       let img=document.createElement('img')
-      let title=document.createElement('h1')
+      let title=document.createElement('h2')
       let category=document.createElement('span')
       let price=document.createElement('p')
+
+      let view=document.createElement('button')
+      view.classList.add('view')
+      let add=document.createElement('button')
+      add.classList.add('add')
+
+      view.textContent='View details'
+      add.textContent='Add to Cart'
 
       img.setAttribute('src',`${product.image}`)
       title.textContent=`${product.title}`
       category.textContent=`${product.category}`
       price.textContent=`$${product.price}`
 
-      forCard.append(title,category,price)
+      cardButtons.append(view,add)
+      cardInfo.append(title,category,price,cardButtons)
 
-      card.append(img,forCard)
-      parentCarts.append(card)
+      card.append(img,cardInfo)
+      
+    parentCarts.append(card)
+  
     }); 
+    })
+.catch(err=>{
+    console.log('Error'+err)
 })
+
