d4bdf334cb94155b9ad21f86775e7d722d6666d7
s1ddiq777 <sidprosto77@gmail.com>
2026-08-25 17:34:03 +0500

    Update index and html

--- index.js
+++ index.js
@@ -1,48 +1,5 @@
+import { createCard } from "./createCard.js"
 const parentCard = document.querySelector('.parent-cards')
-
-function createCard(product) {
-    const card = document.createElement('div')
-    card.classList.add('card')
-
-    const infoForCard = document.createElement('div')
-    infoForCard.classList.add('info-for-card')
-    const buttonsCard = document.createElement('div')
-    buttonsCard.classList.add('buttons-card')
-
-    const img = document.createElement('img')
-    img.classList.add('card-img')
-    const title = document.createElement('div')
-    title.classList.add('card-title')
-    const rate = document.createElement('p')
-    const price = document.createElement('p')
-    price.classList.add('card-price')
-
-    const detailsBtn = document.createElement('div')
-    detailsBtn.classList.add('details-btn')
-    const addBasketBtn = document.createElement('div')
-    addBasketBtn.classList.add('add-basket-btn')
-
-    detailsBtn.textContent = 'Детали'
-    addBasketBtn.textContent = 'В корзину'
-
-    img.setAttribute('src', `${product.image}`)
-
-    if(product.title.length >60){
-         title.textContent=`${product.title.slice(0, 60)}...`
-      }
-      else{
-        title.textContent=`${product.title}`
-      }
-
-    rate.innerHTML = `<span style="color:var(--color-star)">★</span> ${product.rating.rate}`
-    price.textContent = `₽${Math.round(product.price).toLocaleString('ru-RU')}`
-
-    buttonsCard.append(detailsBtn, addBasketBtn)
-    infoForCard.append(title, rate, price)
-    card.append(img, infoForCard, buttonsCard)
-    parentCard.append(card)
-}
-
 fetch('https://fakestoreapi.com/products')
     .then(res => {
         if (!res.ok) {
@@ -52,7 +9,8 @@ fetch('https://fakestoreapi.com/products')
     })
     .then(products => {
         products.forEach(product => {
-            createCard(product)
+            const card = createCard(product)
+            parentCard.append(card)
         })
     })
     .catch(err => {
