98541807347b4f0890233a9d305c6e6da6f4fa52
s1ddiq777 <sidprosto77@gmail.com>
2026-08-26 20:29:59 +0500

    Add logic to price and category selectors

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 import { createCard } from "./createCard.js"
-const parentCard = document.querySelector('.parent-cards')
-fetch('https://fakestoreapi.com/products')
-    .then(res => {
-        if (!res.ok) {
-            throw new Error('Error')
-        }
-        return res.json()
-    })
-    .then(products => {
-        products.forEach(product => {
-            const card = createCard(product)
-            parentCard.append(card)
-        })
-    })
-    .catch(err => {
-        console.log(err)
-    })
+import { fetchProducts } from "./fetchProducts.js"
+fetchProducts()
